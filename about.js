56d09b215e936ba4c5778700858963c65995f37f
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2024-11-21 21:44:29 +0100

    created name-list

--- about.js
+++ about.js
@@ -1,11 +1,10 @@
-//I don't really know how to do it but istead of adding a new div for every person, perhaps we could add the content in JS?
+//I don't really know how to do it but istead of adding a new div for every person, we could add the content in JS?
 
 //Idea:
 
-function Person(item, name, text) {
-    this.item = item;
+function Person(name, aboutText) {
     this.name = name;
-    this.text = text;
+    this.aboutText = aboutText;
 }
 
-let emelie = new Person(teamItem, "Emelie B. Silfverberg", "text");
+let emelie = new Person("Emelie B. Silfverberg", "text");
