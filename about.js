85476cc0b161ce0dc293ee638b6183dd7a676608
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2024-11-21 17:32:07 +0100

    Added HTML CSS Code Created JS Code

---
+++ about.js
@@ -0,0 +1,11 @@
+//I don't really know how to do it but istead of adding a new div for every person, perhaps we could add the content in JS?
+
+//Idea:
+
+function Person(item, name, text) {
+    this.item = item;
+    this.name = name;
+    this.text = text;
+}
+
+let emelie = new Person(teamItem, "Emelie B. Silfverberg", "text");
