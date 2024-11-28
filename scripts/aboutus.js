9fef93e0c9f461ef2382810547a3eab4e868b36b
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2024-11-28 18:14:51 +0100

    JS images added

--- scripts/aboutus.js
+++ scripts/aboutus.js
@@ -1,37 +1,27 @@
-
-function Person(name, aboutText, tab, image) {
-    this.name        =  name;
-    this.aboutText   =  aboutText;
-    this.tab         =  tab;
-    this.image       =  image;
+function Person(name, aboutText, tab, image, imageName) {
+    this.name = name;
+    this.aboutText = aboutText;
+    this.tab = tab;
+    this.image = image;
+    this.imageName = imageName;
 }
 
-
 const teamMembers = [
-    new Person("Our Team!", 
-        "information about the team, about the project, etc. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", 
-        "The Team", ""),
-
-    new Person("Emelie B. Silfverberg", 
-        "Emelie is uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-        "Emelie", "url(../assets/images/reptiles/reptile-hawksbill-turtle.jpg)"),
-
-    new Person("Jordan B. Smith", 
-        "Jordan is an expert in backend systems.", 
-        "Jordan"),
-
-    new Person("Rex T. Lee", 
-        "Rex specializes in frontend and UI/UX design.", 
-        "Rex"),
-    new Person("Pieck F. Inger", 
-        "Rex specializes in frontend and UI/UX design.", 
-        "Pieck"),
-    
+    new Person(
+        "Our Team!",
+        "Information about the team, about the project, etc. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+        "The Team",
+        "assets/images/mammals/mammal-wombat.jpeg",
+        "Our team picture"
+    ),
+    new Person(
+        "Emelie B. Silfverberg",
+        "Emelie is uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Emelie",
+        "assets/images/mammals/mammal-bilby.jpg",
+        "Emelie picture"
+    ),
 ];
-
-// testing
-
-let teamTabs = Array.from(document.querySelectorAll(".tab"));
 
 
 const createTabs = (members) => {
@@ -45,34 +35,12 @@ const createTabs = (members) => {
         
         tab.addEventListener("click", () => {
             setActiveTab(tab);
-            updateTeamContent(member);
+            updateTeamContent(member); 
         });
 
         tabsContainer.appendChild(tab);
     });
 };
-
-// work on image function
-// let teamImages = Array.from(document.querySelectorAll(".img"));
-
-
-// const createImages = (members) => {
-//     const imgageContainer = document.querySelector(".team");
-
-//     members.forEach((member) => {
-//         const image = document.createElement("div");
-//         image.classList.add("img");
-//         image.textContent = member.tab;
-
-        
-//         image.addEventListener("click", () => {
-//             setActiveTab(tab);
-//             updateTeamContent(member);
-//         });
-
-//         tabsContainer.appendChild(image);
-//     });
-// };
 
 
 const setActiveTab = (activeTab) => {
@@ -80,35 +48,42 @@ const setActiveTab = (activeTab) => {
     activeTab.classList.add("active");
 };
 
-const updateTeamContent = (person) => {
-//     let memberWrapper = document.createElement("div");
-//     memberWrapper.classList.add("team__container");
 
-//    let imgContainer = document.createElement("img");
-//    imgContainer.src ="person.image"
-//    imgContainer.alt="person.name picture"
+const updateTeamContent = (member) => {
+    const teamSection = document.querySelector(".team");
 
-//    memberWrapper.appendChild(imgContainer);
+    while (teamSection.firstChild) {
+        teamSection.removeChild(teamSection.firstChild);
+    }
 
-//     let teamName = document.createElement("h3")
-//     teamName.textContent = person.name;
+    const imgContainer = document.createElement("div");
+    const personImage = document.createElement("img");
+    personImage.src = member.image || "../assets/images/default-team.jpg"; // Default image if missing
+    personImage.alt = member.imageName || "Team member image";
+    personImage.classList.add("team__image");
+    imgContainer.appendChild(personImage);
 
-//     let teamSection = document.querySelector("team");
-//     teamSection.appendChild(memberWrapper);
+    const memberWrapper = document.createElement("div");
+    memberWrapper.classList.add("team__container");
 
-    
+    const teamName = document.createElement("h3");
+    teamName.classList.add("team__name");
+    teamName.textContent = member.name;
 
-    document.querySelector(".team__title-name").textContent = person.name;
-    document.querySelector(".team__about-text").textContent = person.aboutText;
-    document.getElementById("#img").style.backgroundImage = person.image;
+    const teamText = document.createElement("p");
+    teamText.classList.add("team__text");
+    teamText.textContent = member.aboutText;
+
+    memberWrapper.appendChild(teamName);
+    memberWrapper.appendChild(teamText);
+
+    teamSection.appendChild(imgContainer);
+    teamSection.appendChild(memberWrapper);
 };
 
 createTabs(teamMembers);
-
 
 if (teamMembers.length > 0) {
     document.querySelectorAll(".tab")[0].classList.add("active");
     updateTeamContent(teamMembers[0]);
 }
-
-
