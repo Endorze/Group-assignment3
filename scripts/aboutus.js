eec5c4c6394b6c18af7c8d287783f068f68fabc0
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2024-12-04 14:28:52 +0100

    responsive CSS grid template

--- scripts/aboutus.js
+++ scripts/aboutus.js
@@ -15,7 +15,7 @@ function Person(name, aboutText, tab, image, imageName, age, interest, origin, s
 const teamMembers = [
     new Person(
         "Team Capybara",
-        "On this page you can read more about each and one of the team members of team Capybara!",
+        "On this page you can read more about each and one of the team members of team Capybara! Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
         "The Team",
         "assets/images/mammals/mammal-wombat.jpeg",
         "Our team picture",
@@ -127,10 +127,15 @@ const updateTeamContent = (person) => {
         teamSection.removeChild(teamSection.firstChild);
     }
     
-    const teamImage = document.createElement("img");
-    teamImage.src = person.image || "../assets/images/default-team.jpg"; // Default image if missing
-    teamImage.alt = person.imageName || "Team image";
+    const teamImage = document.createElement("div");
     teamImage.classList.add("img-info__content", "image");
+        const imageObject = document.createElement("img");
+        imageObject.src = person.image || "../assets/images/default-team.jpg";
+        imageObject.alt = person.imageName || "Team image";
+        imageObject.classList.add("image__child");
+
+        teamImage.appendChild(imageObject)
+    
 
     const teamInfo = document.createElement("div");
     teamInfo.classList.add("img-info__content", "info");
